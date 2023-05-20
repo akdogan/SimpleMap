e9dbb6426043bd9bddf342a5ff3febe7c357c9a1
Arif <c.a.akdogan@googlemail.com>
2023-05-20 14:26:13 +0200

    add coyright notice for osm

--- app/src/main/java/com/akdogan/simplemap/features/mapview/composables/MapCardComposable.kt
+++ app/src/main/java/com/akdogan/simplemap/features/mapview/composables/MapCardComposable.kt
@@ -1,14 +1,19 @@
 package com.akdogan.simplemap.features.mapview.composables
 
 import android.view.LayoutInflater
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import com.akdogan.simplemap.features.mapview.MapStateHolder
 
@@ -34,5 +39,17 @@ fun MapCard(
                 Text("Search here")
             }
         }
+        val uriHandler = LocalUriHandler.current
+        Text(
+            modifier = Modifier
+                .background(Color.White.copy(alpha = 0.7f))
+                .padding(2.dp)
+                .clickable {
+                    uriHandler.openUri("https://www.openstreetmap.org/copyright/en")
+                }
+                .align(Alignment.TopEnd),
+            text = "© OpenStreetMap contributors",
+            style = MaterialTheme.typography.labelSmall
+        )
     }
 }
